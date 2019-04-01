1cbd373b7d770cde010aa988e469f0c44413961a
James Wigdor <jpwigdor@gmail.com>
2019-03-31 22:29:58 -0400

    Make App.js pass props in components

--- client/src/App.js
+++ client/src/App.js
@@ -23,8 +23,19 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={Movie} />
+        <Route
+          path="/"
+          exact
+          render={props => {
+            return <MovieList {...props} />;
+          }}
+        />
+        <Route
+          path="/movies/:id"
+          render={props => {
+            return <Movie {...props} />;
+          }}
+        />
       </div>
     );
   }
